b1321b0698747c198eb061687e64dd292fc6d001
Anniymm <animaisuradze1999@gmail.com>
2025-06-23 23:04:14 +0400

    add for  loops and using recursion versions

---
+++ StringReverse/StringReverser.java
@@ -0,0 +1,28 @@
+package StringReverse;
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+class StringReverser{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the input to reverse ");
+        String input = scanner.next();
+        System.out.println(reverse(input));
+        scanner.close();
+    }
+
+    public static String reverse(String inputi){
+        if (inputi == null || inputi.trim().isEmpty()) {
+            throw new IllegalArgumentException("Input must not be empty or blank");
+        }
+            
+        StringBuilder output = new StringBuilder();
+        char[] chars = inputi.toCharArray();
+
+        for(int i = chars.length - 1; i >= 0;  i --){
+            output.append(chars[i]);
+        }
+        return output.toString();
+        
+    }
+}
